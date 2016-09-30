4c9b98c00f47bf7b498ceb0e9ea9f556433a6de8
Claudius Kerth <claudiuskerth@gmail.com>
2016-09-30 10:42:25 +0100

    modified data.sh
    added test for required programmes;
    added "-b" flag to samtools command for downsampling reads to actually create BAM not SAM;
    added "samtools index" command line to create BAM indeces;
    modified sed command: replace with chr id 11 (not 1)

--- Scripts/data.sh
+++ Scripts/data.sh
@@ -4,14 +4,40 @@
 # file with BAM names $1 is given as input
 
 # set path if not in your /usr/bin
-SAMTOOLS=samtools
-WGET=wget
-GUNZIP=gunzip
-BGZIP=bgzip
-echo Is this your path to samtools? $SAMTOOLS
-echo Is this your path to wget? $WGET
-echo Is this your path to gunzip? $GUNZIP
-echo Is this your path to bgzip? $BGZIP
+SAMTOOLS=`which samtools`
+WGET=`which wget`
+GUNZIP=`which gunzip`
+BGZIP=`which bgzip`
+PARALLEL=`which parallel`
+# echo Is this your path to samtools? $SAMTOOLS
+# echo Is this your path to wget? $WGET
+# echo Is this your path to gunzip? $GUNZIP
+# echo Is this your path to bgzip? $BGZIP
+
+if [ SAMTOOLS == "" ];
+then
+	echo Please install samtools and add it to your PATH environment variable
+fi
+
+if [ WGET == "" ];
+then
+	echo Please install wget and add it to your PATH environment variable.
+fi
+
+if [ GUNZIP == "" ];
+then
+	echo Please install gunzip and add it to your PATH environment variable.
+fi
+
+if [ BGZIP == "" ];
+then
+	echo Please install bgzip and add it to your PATH environment variable.
+fi
+
+if [ PARALLEL == "" ];
+then
+	echo Please install GNU parallel and add it to your PATH environment variable.
+fi
 
 CHROM=11
 
@@ -44,7 +70,7 @@ do
 	do
 		NAME=`echo -n $i | tail -c 58`
 		echo $NAME
-		$SAMTOOLS view -s 0.5 -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/$i $CHROM:61000000-62000000 > Data/$POP.BAMs$NAME 2> /dev/null
+		$SAMTOOLS view -bs 0.5 -h ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/$i $CHROM:61000000-62000000 > Data/$POP.BAMs$NAME 2> /dev/null
 	done
 done
 rm *.bai
@@ -56,12 +82,15 @@ ls Data/LWK.BAMs/*.bam > LWK.bamlist
 ls Data/TSI.BAMs/*.bam > TSI.bamlist
 ls Data/PEL.BAMs/*.bam > PEL.bamlist
 
+# index all .bam files
+cat ALL.bamlist | parallel "samtools index {}"
+
 # download ancestral sequence
 echo Downloading and processing ancestral sequence...
 wget ftp://ftp.ensembl.org/pub/release-65/fasta/ancestral_alleles/homo_sapiens_ancestor_GRCh37_e65.tar.bz
 tar xjf homo_sapiens_ancestor_GRCh37_e65.tar.bz
 cp homo_sapiens_ancestor_GRCh37_e65/homo_sapiens_ancestor_$CHROM.fa Data/tmp.fa
-sed "1s/.*/>1/" Data/tmp.fa > Data/anc.fa
+sed "1s/.*/>11/" Data/tmp.fa > Data/anc.fa
 rm Data/tmp.fa
 $BGZIP Data/anc.fa
 $SAMTOOLS faidx Data/anc.fa.gz
